3d4fbc70d90ebbccba2a1889c219fd6d7d576171
mofalabbasi <mo.f.alabbasi@gmail.com>
2026-09-16 06:51:36 +0300

    Polish site footer and navigation links

--- includes/footer.php
+++ includes/footer.php
@@ -3,52 +3,45 @@
     <div class="row">
       <div class="col-md-6 col-lg-4">
         <div class="info mb-5">
-          <h5 class="text-light"> Books Market </h5>
-          <p class="mb-2"> is your first source of knowledge and reading</p>
+          <h5 class="text-light">Books Market</h5>
+          <p class="mb-2">Your source for reading and knowledge.</p>
           <div class="copyright">
-            Created by <span>Mohammed Alabbasi</span>,Yemen,sana'a
-            <div>&COPY; 2025 - <span>1447</span></div>
+            Created by <span>Mohammed Alabbasi</span>, Yemen, Sana'a
+            <div>&COPY; 2026</div>
           </div>
         </div>
       </div>
-      <div class="col-md-6 col-lg-1">
+      <div class="col-md-6 col-lg-2">
         <div class="links">
           <h5 class="text-light">Links</h5>
           <ul class="list-unstyled">
-            <li>Home</li>
-            <li>Books</li>
-            <li>Cart</li>
-            <li>About</li>
-            <li>Contact</li>
-
+            <li><a href="home.php" class="text-white-50 text-decoration-none">Home</a></li>
+            <li><a href="books.php" class="text-white-50 text-decoration-none">Books</a></li>
+            <li><a href="cart.php" class="text-white-50 text-decoration-none">Cart</a></li>
+            <li><a href="my_orders.php" class="text-white-50 text-decoration-none">My Orders</a></li>
+            <li><a href="about.php" class="text-white-50 text-decoration-none">About</a></li>
+            <li><a href="contact.php" class="text-white-50 text-decoration-none">Contact</a></li>
           </ul>
         </div>
       </div>
       <div class="col-md-6 col-lg-3">
         <div class="links">
           <h5 class="text-light">About us</h5>
-          <!-- <ul class="list-unstyled">
-            <li>Sign In</li>
-            <li>Register</li>
-            <li>About Us</li>
-            <li>Blog</li>
-            <li>contact Us</li>
-          </ul> -->
-          <p>Providing distinctive collections of best-selling books</p>
+          <p>Providing distinctive collections of books and a simple online ordering experience.</p>
         </div>
       </div>
-      <div class="col-md-6 col-lg-4">
+      <div class="col-md-6 col-lg-3">
         <div class="content">
-          <h5 class="text-light">Content Us</h5>
-          <p class="mb-2">Get in touch with us via phone number or email. We are waiting for your call or message </p>
-          <ul class="list-unstyled   mt-2 gap-3">
+          <h5 class="text-light">Contact Us</h5>
+          <p class="mb-2">Get in touch with us by phone or email.</p>
+          <ul class="list-unstyled mt-2">
             <li>
               <i class="fa-solid fa-phone text-light p-2"></i>
-              <span class="ps-1"> 780122441 </span>
+              <span class="ps-1">780122441</span>
             </li>
             <li>
-              <i class="fa-solid fa-envelope  p-2 text-light"></i>
-              <span class="ps-1"> mohammed8alabbasi8@gmail.com </span>
+              <i class="fa-solid fa-envelope p-2 text-light"></i>
+              <span class="ps-1">mohammed8alabbasi8@gmail.com</span>
             </li>
           </ul>
         </div>
