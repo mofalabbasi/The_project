b888385bf29553c810d0c1c3dd6ebfa9137bed5a
mofalabbasi <mo.f.alabbasi@gmail.com>
2026-09-16 06:46:59 +0300

    Clean up about page authentication and content

--- about.php
+++ about.php
@@ -1,64 +1,46 @@
 <?php
-session_start();
-if(!isset($_SESSION['id']))
-{
-  header('refresh:0;index.php');
-}
+require_once "includes/auth.php";
+require_login();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Market Books</title>
+    <title>Books Market - About Us</title>
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/all.min.css" />
     <link rel="stylesheet" href="css/boot.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,500;1,100&display=swap"
-        rel="stylesheet" />
 </head>
-
 <body>
-    <?php
-    if ($_SESSION['role'] == 'admin') {
-        include "includes/adminHeader.php";
-    } else {
-        include "includes/header.php";
-    }
-    ?>
+<?php
+if (($_SESSION['role'] ?? '') === 'admin') {
+    include "includes/adminHeader.php";
+} else {
+    include "includes/header.php";
+}
+?>
 
-    <div class="stuff pt-5 m-5">
-        <div class="container">
-
-            <div class="row align-items-center">
-                <div class="col-lg-6 text-center mb-4 text-md-start">
-                    <div class="text">
-                        <h4>About Us</h4>
-                        <p class="text-black-50 fs-6"> We provide you with various books and novels at the best prices.
-                        </p>
-                        <p class="text-black-50 fs-6"> Order now through the website everything you need to receive it
-                            within a maximum of 24 hours.
-                        </p>
-                        <p class="text-black-50 fs-6"> What distinguishes us from other stores : </p>
-                        <p class="text-black-50 fs-6"> - Our delivery is fast, safe and easy </p>
-                        <p class="text-black-50 fs-6"> - Delivery service is available to all governorates</p>
-                    </div>
+<div class="stuff pt-5 m-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 text-center mb-4 text-md-start">
+                <div class="text">
+                    <h4>About Us</h4>
+                    <p class="text-black-50 fs-6">We provide a selection of books and novels through our online book market.</p>
+                    <p class="text-black-50 fs-6">Customers can browse the available books, add them to a cart, and submit an order through the website.</p>
+                    <p class="text-black-50 fs-6">Our website is designed to make browsing and ordering books simple and convenient.</p>
                 </div>
-                <div class="col-lg-6 text-center">
-
-                    <img class="img-fluid w-75 mb-5 rounded float-end" src="img/7.jpg" alt="" />
-
-
-                </div>
+            </div>
+            <div class="col-lg-6 text-center">
+                <img class="img-fluid w-75 mb-5 rounded float-end" src="img/7.jpg" alt="Books Market" />
             </div>
         </div>
     </div>
-    <?php include "includes/footer.php"; ?>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/all.min.js"></script>
-</body>
+</div>
 
+<?php include "includes/footer.php"; ?>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/all.min.js"></script>
+</body>
 </html>
